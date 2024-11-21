801c80a393e83fc59b2cfe807993c4020d5ec1d5
BeeVali <beevali_ro@yahoo.com>
2024-11-21 02:31:31 +0200

    Ex. 1-7

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Basics PART1
+        //1 Afisare Hello si numele meu
+        /*
+         System.out.println("Hello");
+        System.out.println("Necula Valentin-Cristian");
+        */
+        //2 Suma 2 numere
+        /*
+         int sum=0;
+        Scanner sc = new Scanner(System.in);
+        int a2 = sc.nextInt();
+        int b2= sc.nextInt();
+        sum=a2+b2;
+        System.out.println("The sum of these 2 numbers is " + sum);
+        */
+        //3 Impartirea a 2 numere
+        /*
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int div=a/b;
+        System.out.println(div);
+         */
+        //4 Operatii
+        /*
+        System.out.println(-5+(8*6));
+        System.out.println((55+9)%9);
+        System.out.println(20+(-3*5)/8);
+        System.out.println(5+(15/3*2)-(8%3));
+         */
+        //5 Produsul a 2 numere
+        /*Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int prod=a*b;
+        System.out.println(a+" X " + b+ " = " + prod);
+        */
+        //6 Suma, Scaderea, Inmultirea, Impartirea, Modulus
+        /*
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum=a+b;
+        int scad=a-b;
+        int inm=a*b;
+        int imp=a/b;
+        int mod=a%b;
+        System.out.println(sum + " " + scad + " " + inm + " " + imp + " " + mod);
+         */
+        // 7 n de la tastatura si inmultirea lui de la 1 -> 10
+        /*Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++)
+            System.out.println(n + " x " + i + " = " + n*i);
+         */
+
+
+
+    }
+}
